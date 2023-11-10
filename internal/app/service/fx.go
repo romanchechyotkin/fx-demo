6528fe340f9910b1440f7df6f1f083dbaac1cb19
romanchechyotkin <romanchechyotkin@gmail.com>
2023-11-10 11:10:38 +0300

    init

--- internal/app/service/fx.go
+++ internal/app/service/fx.go
@@ -10,16 +10,22 @@ import (
 func NewModule() fx.Option {
 	return fx.Module(
 		"service",
-		fx.Provide(),
+		fx.Provide(
+			NewService,
+		),
 		fx.Invoke(
-			func(lc fx.Lifecycle, log *zap.Logger) {
+			func(lc fx.Lifecycle, log *zap.Logger, srv *Service) {
 				lc.Append(fx.Hook{
 					OnStart: func(_ context.Context) error {
 						log.Info("service started")
+						go srv.Run()
 						return nil
 					},
 					OnStop: func(_ context.Context) error {
 						log.Info("service stopped")
+
+						srv.worker <- struct{}{}
+
 						return nil
 					},
 				})
--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -1,8 +1,48 @@
 package service
 
-import "go.uber.org/zap"
+import (
+	"context"
+	"fmt"
+	"go.uber.org/zap"
+	"time"
+)
 
 type Service struct {
 	log    *zap.Logger
 	worker chan struct{}
+	store  map[int]struct{}
+}
+
+func NewService(log *zap.Logger) *Service {
+	return &Service{
+		log:    log,
+		worker: make(chan struct{}),
+		store: map[int]struct{}{
+			1: {},
+			2: {},
+			3: {},
+			4: {},
+			5: {},
+		},
+	}
+}
+
+func (s *Service) Run() {
+	workerCtx := context.Background()
+	s.log.Info("worker started")
+
+	for {
+		select {
+		case <-s.worker:
+			workerCtx.Done()
+			return
+		case <-time.After(3 * time.Second):
+			s.log.Info("service job")
+
+			for k, _ := range s.store {
+				fmt.Println(k)
+			}
+		}
+	}
+
 }
